03cac18b9c0626ebc4ec3467dbab2efa6cfa83bd
kevinlai-myob <kevin.lai@myob.com>
2021-04-27 15:10:16 +1000

    add

---
+++ MagicYearCalculator.App.Tests/MagicYearCalculatorTests.cs
@@ -0,0 +1,33 @@
+using Xunit;
+
+namespace MagicYearCalculator.App.Tests
+{
+    public class MagicYearCalculatorShould
+    {   
+        [Fact]
+        public void GetFullname()
+        {
+            var calculator = new MagicYearCalculator();
+            var fullname = calculator.GetFullname("John", "Doe");
+            
+            Assert.Equal("John Doe", fullname);
+        }
+        [Fact]
+        public void GetMonthlySalary()
+        {
+            var calculator = new MagicYearCalculator();
+            var monthlySalary = calculator.GetMonthlySalary(80000);
+            
+            Assert.Equal(6666, monthlySalary);
+        }
+        [Fact]
+        public void GetMagicYear()
+        {
+            var calculator = new MagicYearCalculator();
+            var magicYear = calculator.GetMagicYear(2000);
+            
+            Assert.Equal(2065, magicYear);
+        }
+
+    }
+}
---
+++ MagicYearCalculator/MagicYearCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MagicYearCalculator.App.Tests
+{
+    public class MagicYearCalculator
+    {
+        public string GetFullname(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+
+        public double GetMonthlySalary(double annualSalary)
+        {
+            return Math.Floor(annualSalary / 12);
+        }
+
+        public double GetMagicYear(double magicYear)
+        {
+            return magicYear + 65;
+        }
+    }
+}
